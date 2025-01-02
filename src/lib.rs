85c76589ded3b395c9bce435e14f3cdf5b1f7a59
Advait Shinde <advait.shinde@gmail.com>
2025-01-02 09:42:34 -0800

    maturin_import_hook works.

--- src/lib.rs
+++ src/lib.rs
@@ -16,12 +16,23 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+#[pyfunction]
+fn double(x: usize) -> usize {
+    x * 2
+}
+
+#[pyfunction]
+fn panic() {
+    panic!("This is a panic message");
+}
+
 /// A Python module implemented in Rust. The name of this function must match
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
 /// import the module.
-#[pymodule]
-fn dominionator_rust(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    println!("Inside dominionator_rust module creation");
+#[pymodule(name = "_rust")]
+fn dominionator(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(double, m)?)?;
+    m.add_function(wrap_pyfunction!(panic, m)?)?;
     Ok(())
 }
