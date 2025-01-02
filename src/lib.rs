e0d5bd4cfbb62cf102b8ad0b458f26a5f4540d9a
Advait Shinde <advait.shinde@gmail.com>
2025-01-01 21:10:18 -0800

    Pyo3 hello world working!

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,21 @@ pub mod self_play;
 pub mod state;
 pub mod types;
 pub mod utils;
+
+use pyo3::prelude::*;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+/// A Python module implemented in Rust. The name of this function must match
+/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
+/// import the module.
+#[pymodule]
+fn dominionator_rust(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    println!("Inside dominionator_rust module creation");
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    Ok(())
+}
